6d73f72723d6488dfdb96cbb27b9bcc50f57c1da
minwook-shin <minwook0106@gmail.com>
2024-03-11 23:24:06 +0900

    move package by management efficiency
    f-scheduler repo for better management
    https://github.com/minwook-shin/f-scheduler

--- apps/operator/main.py
+++
@@ -1,8 +0,0 @@
-from core import ConditionOperator, BashOperator
-
-condition_task = ConditionOperator('10 > 1', task_id='condition_task')
-bash_task = BashOperator('ls', task_id='bash_task')
-bash_task2 = BashOperator('ls -al', task_id='bash_task2')
-
-condition_task.next(bash_task).next(bash_task2)
-condition_task.run()
