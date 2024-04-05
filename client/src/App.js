ca8a1844563cc4045561d42301700bac2d236d43
minwook-shin <minwook0106@gmail.com>
2024-04-05 19:59:43 +0900

    change the task items default value

--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,17 @@ function App() {
   const [taskItems, setTaskItems] = useState([]);
 
   const initialTaskItems = useMemo(() => [
-    { text: "scroll_up", time: '' },
-    { text: "scroll_down", time: '' },
-    { text: "single_click", x: "{{_local['top_left_x']}}", y: "{{_local['top_left_y']}}", time: 1 },
-    { text: "long_press", x: "{{_local['top_left_x']}}", y: "{{_local['top_left_y']}}", time: 1 },
-    { text: "key_event", key_event: '', time: '' },
-    { text: "delay", time: '' },
-    { text: "loop", time: '', functions: [] },
-    { text: "input_text", time: '', input_text: '' },
-    { text: "screen_capture", time: '' },
+    { text: "scroll_up", time: 1 },
+    { text: "scroll_down", time: 1 },
+    { text: "single_click", x: '', y: '', time: 1 },
+    { text: "long_press", x: '', y: '', time: 1 },
+    { text: "key_event", key_event: '', time: 1 },
+    { text: "delay", time: 1 },
+    { text: "loop", time: 1, functions: [] },
+    { text: "input_text", time: 1, input_text: '' },
+    { text: "screen_capture", time: 1 },
     { text: "image_matching", time: 1, template: ''},
-    { text: "extract_text", time: 1, top_left_x: "{{_local['top_left_x']}}", top_left_y: "{{_local['top_left_y']}}", bottom_right_x: "{{_local['bottom_right_x']}}", bottom_right_y: "{{_local['bottom_right_y']}}"},
+    { text: "extract_text", time: 1, top_left_x: '', top_left_y: '', bottom_right_x: '', bottom_right_y: ''},
     { text: "user_variable", time: 1, variable_name: '', variable_value: ''}
   ], []);
 
