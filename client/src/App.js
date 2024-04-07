dfe3b75bbc0c6941e02ce81ce2b7f6deae16c783
minwook-shin <minwook0106@gmail.com>
2024-04-07 11:05:26 +0900

    change design to render only visible task items on screen

--- client/src/App.js
+++ client/src/App.js
@@ -13,18 +13,18 @@ function App() {
   const [dragCoords, setDragCoords] = useState({ top_left_x: 0, top_left_y: 0, bottom_right_x: 0, bottom_right_y: 0 });
 
   const initialTaskItems = useMemo(() => [
-    { text: "scroll_up", time: 1 },
-    { text: "scroll_down", time: 1 },
-    { text: "single_click", x: '', y: '', time: 1 },
-    { text: "long_press", x: '', y: '', time: 1 },
-    { text: "key_event", key_event: '', time: 1 },
-    { text: "delay", time: 1 },
-    { text: "loop", time: 1, functions: [] },
-    { text: "input_text", time: 1, input_text: '' },
-    { text: "screen_capture", time: 1 },
-    { text: "image_matching", time: 1, template: '' },
-    { text: "extract_text", time: 1, top_left_x: '', top_left_y: '', bottom_right_x: '', bottom_right_y: '' },
-    { text: "user_variable", time: 1, variable_name: '', variable_value: '' }
+    { display_text:"위로 스크롤", text: "scroll_up", time: 1 },
+    { display_text:"아래로 스크롤", text: "scroll_down", time: 1 },
+    { display_text:"단일 클릭", text: "single_click", x: '', y: '', time: 1 },
+    { display_text:"길게 클릭", text: "long_press", x: '', y: '', time: 1 },
+    { display_text:"키 이벤트 입력", text: "key_event", key_event: '', time: 1 },
+    { display_text:"지연 시간", text: "delay", time: 1 },
+    { display_text:"반복 작업", text: "loop", time: 1, functions: [] },
+    { display_text:"텍스트 키보드 입력", text: "input_text", time: 1, input_text: '' },
+    { display_text:"안드로이드 화면 캡쳐", text: "screen_capture", time: 1 },
+    { display_text:"이미지 위치 클릭", text: "image_matching", time: 1, template: '' },
+    { display_text:"지정 범위 텍스트 추출", text: "extract_text", time: 1, top_left_x: '', top_left_y: '', bottom_right_x: '', bottom_right_y: '' },
+    { display_text:"시나리오 사용자 변수 부여", text: "user_variable", time: 1, variable_name: '', variable_value: '' }
   ], []);
 
   useEffect(() => {
--- client/src/TaskList.js
+++ client/src/TaskList.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList } from 'react-window';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator'; // 아이콘 임포트
 
-function TaskList({ taskItems}) {
+function TaskList({ taskItems }) {
+    const handleDragStart = (event, item) => {
+        event.dataTransfer.setData("text/plain", JSON.stringify(item));
+    };
 
-  const handleDragStart = (event, item) => {
-    event.dataTransfer.setData("text/plain", JSON.stringify(item));
-  };
+    const renderRow = (props) => {
+        const { index, style } = props;
+        const item = taskItems[index];
+        return (
+            <ListItem
+                key={item.id}
+                style={{ ...style, border: '0.5px solid #ddd', borderLeft: 'none', borderRight: 'none' }}
+                draggable
+                onDragStart={(event) => handleDragStart(event, item)}
+            >
+                <ListItemText primary={item.display_text} />
+            </ListItem>
+        );
+    };
+
     return (
-        <div className="task">
-            <h2>사용 가능한 작업 (끌기)</h2>
-            <ul>
-                {taskItems.map(item => (
-                    <li
-                        key={item.id}
-                        draggable
-                        onDragStart={(event) => handleDragStart(event, item)}
-                    >
-                        {item.text}
-                    </li>
-                ))}
-            </ul>
+        <div className="task" style={{ width: '100%' }}>
+            <h2> <DragIndicatorIcon /> 사용 가능한 작업</h2>
+            <FixedSizeList
+                height={350}
+                width="100%"
+                itemSize={40}
+                itemCount={taskItems.length}
+                children={renderRow}
+            />
         </div>
     );
 }
