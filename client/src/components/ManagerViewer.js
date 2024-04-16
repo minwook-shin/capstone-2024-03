e08089891ad02ec90eb3da121c8e1d550a4e96f0
minwook-shin <minwook0106@gmail.com>
2024-04-16 22:01:04 +0900

    add jsdoc and formatting code for manager, contorl box

--- client/src/components/ManagerViewer.js
+++ client/src/components/ManagerViewer.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 const { ipcRenderer } = window;
 
+/**
+ * 결과 매니저 창을 띄우는 함수
+ * @param {*} _ 
+ */
 export const handleClickShowManager = (_) => {
-    ipcRenderer.send("show_manager");
+  ipcRenderer.send("show_manager");
 };
 
 function ManagerViewer() {
-    return (
-        <></>
-    );
+  return <></>;
 }
 
 export default ManagerViewer;
