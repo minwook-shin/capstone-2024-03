f4e3e87ecfb1db64e3e27de5734398b5435d9eb4
minwook-shin <minwook0106@gmail.com>
2024-04-18 22:11:45 +0900

    add image, vm api template

--- apps/image-api/main.py
+++ apps/image-api/main.py
@@ -6,7 +6,20 @@ from blueprints.image_processing import ProcessImage
 app = Flask(__name__)
 
 app.register_blueprint(ProcessImage)
-swagger = Swagger(app)
+template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "IMAGE API",
+    "description": "이미지 처리하는 API",
+    "contact": {
+      "responsibleOrganization": "kookmin-sw",
+      "responsibleDeveloper": "minwook-shin",
+      "url": "https://github.com/kookmin-sw/capstone-2024-03",
+    },
+    "version": "1.0.0"
+  },
+}
+swagger = Swagger(app, template=template)
 
 
 @app.after_request
--- apps/vm-api/main.py
+++ apps/vm-api/main.py
@@ -5,7 +5,20 @@ from flask_variable_manager import VariableManagerExtension
 vm = VariableManagerExtension()
 app = Flask(__name__)
 vm.init_app(app)
-swagger = Swagger(app)
+template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "Variable API",
+    "description": "변수 매니저 & 파이썬 스크립트 API",
+    "contact": {
+      "responsibleOrganization": "kookmin-sw",
+      "responsibleDeveloper": "minwook-shin",
+      "url": "https://github.com/kookmin-sw/capstone-2024-03",
+    },
+    "version": "1.0.0"
+  },
+}
+swagger = Swagger(app, template=template)
 
 
 @app.after_request
